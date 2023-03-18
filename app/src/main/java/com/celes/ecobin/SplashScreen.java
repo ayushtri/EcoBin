d26cd146d0a5d615bc73f100ce5365351fb00a20
Ayush Tripathi <tripathi.at02@gmail.com>
2023-03-18 16:07:14 +0530

    EcoBin Minor Project

--- app/src/main/java/com/celes/ecobin/SplashScreen.java
+++ app/src/main/java/com/celes/ecobin/SplashScreen.java
@@ -2,7 +2,9 @@ package com.celes.ecobin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //multithreading for splash screen time limit
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish(); //pop activity from stack
+            }
+        },1000);
+
+
     }
 }
